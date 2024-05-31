40991610f9a58834186aa4d94545762993028b42
edwilson <edwilsonfa@gmail.com>
2024-05-31 14:13:36 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,40 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+/*        System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developoer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcap(bootcamp);
+        System.out.println("Conteudos Inscritos Camila: "+devCamila.getConteudosInscritos());
+
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Camila: "+devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Camila: "+devCamila.getConteudosConcluidos());
+        System.out.println("XP "+devCamila.calcularTotalXp());
+
+        System.out.println("-------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcap(bootcamp);
+        System.out.println("Conteudos Inscritos Joao: "+devJoao.getConteudosInscritos());
+
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Joao: "+devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Joao: "+devJoao.getConteudosConcluidos());
+        System.out.println("XP "+devJoao.calcularTotalXp());
     }
 }
